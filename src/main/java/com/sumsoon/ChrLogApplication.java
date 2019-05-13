36489eac1e0050582d37e0f94c40868a80bf2c22
cyl5yy412 <cyl5yy412@gmail.com>
2019-05-13 21:14:55 +0800

    init

--- src/main/java/com/sumsoon/ChrLogApplication.java
+++ src/main/java/com/sumsoon/ChrLogApplication.java
@@ -1,9 +1,13 @@
 package com.sumsoon;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.sumsoon"})
+@MapperScan("com.sumsoon.zzz.mapper")
 public class ChrLogApplication {
 
 	public static void main(String[] args) {
